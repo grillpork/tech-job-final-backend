85b1156d2f2277bd9131472c3e4548dbc56c00f1
Tripop Saibut <tripopsaibut@gmail.com>
2025-09-30 22:06:46 +0700

    update jwt

--- src/plugins/jwt.ts
+++ src/plugins/jwt.ts
@@ -6,9 +6,6 @@ import fastifyJwt from '@fastify/jwt';
 import { config } from '../config';
 
 async function jwtPlugin(server: FastifyInstance) {
-  // เพิ่ม log ตรงนี้เพื่อดูว่า plugin ได้รับ secret จริงหรือไม่
-  console.log('🔒 Initializing JWT plugin with secret:', config.JWT_SECRET);
-
   server.register(fastifyJwt, {
     secret: config.JWT_SECRET,
   });
